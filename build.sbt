dcaf7b8bc1c810bef326419ab70241bcde820af5
cheluvesha <cheluvesh.b@gmail.com>
2020-11-25 20:36:30 +0530

    [CHELUVESHA]Added: PlayAverage

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "PracticeSpace"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+
+libraryDependencies += "org.apache.hadoop" % "hadoop-core" % "1.2.1"
---
+++ src/main/scala/PlayAverage.scala
@@ -0,0 +1,61 @@
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.Mapper
+import org.apache.hadoop.mapreduce.Reducer
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+
+import scala.collection.JavaConversions._
+
+object PlayAverage  {
+
+  class PlayTimeMapper extends Mapper[Object,Text,Text,LongWritable] {
+    val one = new LongWritable(1)
+    val word = new Text("PlayTime(Sec)")
+
+    override
+    def map(key:Object, value:Text, context:Mapper[Object,Text,Text,LongWritable]#Context): Unit = {
+          val startTime = Calendar.getInstance()
+          val endTime = Calendar.getInstance()
+          val dataFormat = new SimpleDateFormat("yyyy/mm/dd HH:mm:ss")
+          val dataSet = value.toString.split(",")
+          startTime.setTime(dataFormat.parse(dataSet(4)))
+          endTime.setTime(dataFormat.parse(dataSet(5)))
+          val playTime: Long = (endTime.getTimeInMillis - startTime.getTimeInMillis) / 1000
+          val values = new LongWritable(playTime)
+          context.write(word,values)
+        }
+  }
+  class PlayTimeReducer extends Reducer[Text,LongWritable,Text,LongWritable] {
+    override
+    def reduce(key:Text, values:java.lang.Iterable[LongWritable], context:Reducer[Text,LongWritable,Text,LongWritable]#Context): Unit = {
+      var avg: Long = 0
+      var sum: Long = 0
+      var cnt: Long = 0
+      for(value <- values) {
+        sum += value.get()
+        cnt += 1
+      }
+      avg = sum / cnt
+      context.write(key, new LongWritable(avg))
+    }
+  }
+  def main (args: Array[String]): Unit ={
+    val conf = new Configuration()
+    val job = new Job(conf, "word count")
+    job.setJarByClass(classOf[PlayTimeMapper])
+    job.setMapperClass(classOf[PlayTimeMapper])
+    job.setCombinerClass(classOf[PlayTimeReducer])
+    job.setReducerClass(classOf[PlayTimeReducer])
+    job.setOutputKeyClass(classOf[Text])
+    job.setOutputValueClass(classOf[LongWritable])
+    FileInputFormat.addInputPath(job, new Path("input"))
+    FileOutputFormat.setOutputPath(job, new Path("output"))
+    if (job.waitForCompletion(true)) 0 else 1
+  }
+}
